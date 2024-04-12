c11c74125d6efeb73412dcab9357bceb7c528aec
arounamounchili <virtual.power.virtual@gmail.com>
2024-04-12 06:46:18 +0200

    create EntityManager class

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -5,22 +5,54 @@
 
 using IdType = uint32_t;
 
-class Entity
-{
-public:
-    Entity(IdType id) : m_id{id} {};
-    Entity(const Entity &) = default;
-    Entity &operator=(const Entity &) = default;
+class World;
+class EntityManager;
 
-    IdType getId() const { return m_id; }
+class Entity {
+ public:
+  Entity(IdType id) : m_id{id} {};
+  Entity(const Entity &) = default;
+  Entity &operator=(const Entity &) = default;
 
-    bool operator==(const Entity &e) const { return getId() == e.getId(); }
-    bool operator!=(const Entity &e) const { return getId() != e.getId(); }
-    bool operator<(const Entity &e) const { return getId() < e.getId(); }
-    bool operator>(const Entity &e) const { return getId() > e.getId(); }
+  IdType getId() const { return m_id; }
 
-private:
-    IdType m_id;
+  bool operator==(const Entity &e) const { return getId() == e.getId(); }
+  bool operator!=(const Entity &e) const { return getId() != e.getId(); }
+  bool operator<(const Entity &e) const { return getId() < e.getId(); }
+  bool operator>(const Entity &e) const { return getId() > e.getId(); }
+
+  template <typename T, typename... Args>
+  void addComponent(Args &&...args);
+  template <typename T>
+  void removeComponent() const;
+  template <typename T>
+  bool hasComponent() const;
+  template <typename T>
+  T &getComponent() const;
+
+ private:
+  IdType m_id;
+  EntityManager *entityManager = nullptr;
 };
+
+class EntityManager {
+ public:
+  EntityManager(World &world) : m_world{world} {};
+
+ private:
+  World &m_world;
+};
+
+template <typename T, typename... Args>
+void Entity::addComponent(Args &&...args) {}
+
+template <typename T>
+void Entity::removeComponent() const {}
+
+template <typename T>
+bool Entity::hasComponent() const {}
+
+template <typename T>
+T &Entity::getComponent() const {}
 
 #endif
