31d05abbf5e93b969d1dcb208d70d214571bf26c
arounamounchili <virtual.power.virtual@gmail.com>
2024-04-12 08:14:18 +0200

    Create a Pool class

---
+++ src/Pool.hpp
@@ -0,0 +1,85 @@
+#ifndef POOL_HPP
+#define POOL_HPP
+
+#include <vector>
+
+#include "Config.hpp"
+
+namespace SimpleECS
+{
+    // Requires to have a vector pools containing different object types
+    class AbstractPool
+    {
+    public:
+        virtual ~AbstractPool() = default;
+        virtual void clear() = 0;
+    };
+
+    // A Pool is just a vector (contiguous data) of objects of type T
+    template <typename T>
+    class Pool : public AbstractPool
+    {
+    public:
+        Pool(int capacity = 100)
+        {
+            Resize(capacity);
+        }
+
+        virtual ~Pool() = default;
+
+        bool IsEmpty() const
+        {
+            return m_data.empty();
+        }
+
+        unsigned int GetSize() const
+        {
+            return m_data.site();
+        }
+
+        void Resize(int n)
+        {
+            m_data.resize(n);
+        }
+
+        void Set(IdType entittyId, T object)
+        {
+            m_data[entittyId] = object;
+        }
+
+        T& Get(IdType entityId)
+        {
+            return static_cast<T&>(m_data[entityId]);
+        }
+
+        void Add(T object)
+        {
+            m_data.push_back(object);
+        }
+
+        T& operator[](IdType entityId)
+        {
+            return m_data[entityId];
+        }
+
+        const T& operator[](IdType entityId) const
+        {
+            return m_data[entityId];
+        }
+
+        std::vector<T> GetData()
+        {
+            return m_data;
+        }
+
+        void clear() override
+        {
+            m_data.clear();
+        }
+
+    private:
+        std::vector<T> m_data;
+    };
+}  // namespace SimpleECS
+
+#endif
